73c76fdbe2c56d43195d976c2cd1713adbfaae3b
Cristóbal González <cristobalj.gonzalez@gmail.com>
2025-10-08 12:24:59 -0300

    Validation function

---
+++ validation.py
@@ -0,0 +1,12 @@
+import json
+from jsonschema import validate, ValidationError
+
+with open('schema.json', 'r') as f:
+    CREATURE_SCHEMA = json.load(f)
+
+def validate_creature_data(data):
+    try: 
+        validate(instance=data, schema=CREATURE_SCHEMA)
+        return True, None
+    except ValidationError as e:
+        return False, e.message
